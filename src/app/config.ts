1deb3af7be9ec56a622db091e30a4afc81524863
thaiwire <thaiwireproducts1@gmail.com>
2025-09-29 10:41:43 +0700

    update

---
+++ src/app/config.ts
@@ -0,0 +1,5 @@
+export const config = {
+  apiUrl: "https://localhost:3001/api",
+  tokenKey: "bun_service",
+};
+export default config;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,93 @@
-import Image from "next/image";
+"use client";
+import Swal from "sweetalert2";
+import { config } from "./config";
+import { useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      if (!username || !password) {
+        Swal.fire({
+          icon: "warning",
+          title: "Missing Fields",
+          text: "Please enter both username and password.",
+        });
+        return;
+      }
+      const payload = { username, password };
+
+      const response = await axios.post(
+        `${config.apiUrl}/api/user/login`,
+        payload
+      );
+
+      if (response.data.token !== undefined) {
+        localStorage.setItem(config.tokenKey, response.data.token);
+        router.push("/dashboard");
+      }
+    } catch (error: any) {
+      Swal.fire({
+        icon: "error",
+        title: "Login Failed",
+        text:
+          error.response?.data?.message || "An error occurred during login.",
+      });
+    }
+  };
+
+  return (
+    <div
+      className="flex flex-col items-center justify-center min-h-screen 
+    bg-gradient-to-br from-gray-800 to-gray-950"
+    >
+      <div className="text-gray-400 text-4xl font-bold mb-10">
+        ระบบ BunService 2025
+      </div>
+      <div
+        className="bg-gray-800 p-8 rounded-2xl shadow-xl w-full
+      max-w-md"
+      >
+        <h1 className="text-2xl font-bold mb-2 text-white">เข้าสู่ระบบ</h1>
+        <form className="flex flex-col gap-2 mt-4 w-72" onSubmit={handleLogin}>
+          <div className="text-white">
+            <i className="fa fa-user mr-2"></i>
+            Username
+          </div>
+          <input
+            type="text"
+            className="form-control mt-5 text-xl"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <div className="text-white mt-5">
+            <i className="fa fa-lock mr-2"></i>
+            Password
+          </div>
+          <input
+            type="password"
+            className="form-control mt-4 text-xl"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button
+            type="submit"
+            className="bg-blue-600 hover:bg-blue-700 text-white
+            font-bold py-2 px-4 rounded mt-5 text-xl"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+            <i className="fa fa-sign-in-alt mr-2"></i>
+            Sign In
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
